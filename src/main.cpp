bfc851e5e78c6075f793e81ccf323c1c166f4af3
Anubhav Gautam <anubhavgautam9720169985@gmail.com>
2026-02-19 11:06:38 +0530

    GIT ADD.

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@ void setup() {
     pinMode(touchPin, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.begin(9600);
-
-
 }
 
 void loop() {
-// write your code here
+    int state = digitalRead(touchPin);
+
 }
