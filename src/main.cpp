442a6701feb364481668835706cd428545fa18b6
Anubhav Gautam <anubhavgautam9720169985@gmail.com>
2026-02-19 11:05:12 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+int touchPin = 3;
+void setup() {
+    pinMode(touchPin, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
+}
+
+void loop() {
+// write your code here
+}
